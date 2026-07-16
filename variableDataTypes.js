a20e9f3e15c77ebf8728c72f4162a0b6274079e6
buntyayadav2003-source <buntyayadav2003@gmail.com>
2026-07-16 18:26:37 +0530

    variable

---
+++ variableDataTypes.js
@@ -0,0 +1,24 @@
+let  sourabhJ = Symbol ('sourabh')
+let  sourabhS = Symbol ('sourabh')
+
+
+let sourabhj = "developer"; 
+let sourabhS = "developer";
+
+console.log(sourabhj == sourabhS); 
+
+x = 5
+y = 5
+ console.log(x == y);
+ 
+// the Locked Box
+
+const car = {
+    brand: "verna",
+    color: "black"
+};
+
+car.color = "black"
+car. year = 2024;
+
+console.log(car);
