bf2c39b664d999f120a17c3ea204e54e3f3c6f1c
buntyayadav2003-source <buntyayadav2003@gmail.com>
2026-07-16 18:02:56 +0530

    Exepression

---
+++ Exepressionop.js
@@ -0,0 +1,71 @@
+let num1 = 10;
+let num2 = 5;
+
+
+console.log("Addition:", num1 + num2);
+console.log("subtraction:", num1 - num2);
+console.log("multiplication:", num1 * num2);
+console.log("Divsion:", num1 / num2);
+console.log("Modulus:", num1 % num2);
+
+
+
+
+let a = 10, b = 20;
+
+
+console.log(a > b); 
+console.log(a < b);
+console.log(a === b);
+console.log(a !== b);
+
+//combine logical operators
+
+let isAdult = true;
+let hasLicense = false;
+
+console.log(isAdult && hasLicense);
+console.log(isAdult || hasLicense);
+console.log(!isAdult);
+
+  //demonstrate op
+
+  let result = 10 + 2 * 5 - 8 / 4;
+  console.log(result);
+
+
+
+  // assignment
+  
+  let x = 10;
+  x += 5; 
+  console.log(x);
+
+  x *= 2;
+  console.log(x);
+  
+  
+// Arithmetic Operation
+
+let firstNumber = Number(prompt("Enter the first number"))
+let secondNumber = Number(prompt("Enter the second number "))
+  
+
+console.log("Enter the first Number: " + firstNumber);
+console.log("Enter the second Number: " + secondNumber);
+
+
+
+console.log("Addition: " + (firstNumber + secondNumber));
+
+console.log("subtraction: " + (firstNumber - secondNumber));
+
+console.log("Multiplication: " + (firstNumber * secondNumber));
+
+console.log("Division: " + (firstNumber / secondNumber));
+
+console.log("Modulus: " + (firstNumber % secondNumber));
+
+
+
+
