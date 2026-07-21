05c59e50feabedd1b30a3fc3a01089a9852a3354
buntyayadav2003-source <buntyayadav2003@gmail.com>
2026-07-21 14:23:21 +0530

    variabledatatype

---
+++ datatypevariable.js
@@ -0,0 +1,143 @@
+
+let sourabhJ_symbol = Symbol('sourabh');
+let sourabhS_symbol = Symbol('sourabh');
+
+console.log(sourabhJ_symbol == sourabhS_symbol); 
+
+
+let sourabhJ = "developer";
+let sourabhS = "developer";
+
+console.log(sourabhJ == sourabhS); 
+
+
+
+// 4. Object 
+const car = {
+    brand: "verna",
+    color: "black"
+};
+
+car.color = "black";
+car.year = 2024;
+
+console.log(car);
+
+// 5. Number
+let age = 10;
+let temperature = -5;
+console.log(temperature);
+console.log(age);
+  
+let x = 10;
+let y = 5;
+
+console.log("Sum of x and y is:", x + y);
+console.log("Difference is:", x - y);
+console.log("products is:", x * y);
+console.log("Quotient is:", x / y);
+
+
+
+
+
+
+// string
+ let name = "spiderman";
+ let message ="Hello, how are you 😊"
+   console.log(name);
+   console.log(message);
+   
+   
+//Booleans
+
+let isSuuny = true;
+let isTires = false;
+console.log(isSuuny);
+console.log(isTires);
+
+
+let isHungry = true;
+ 
+
+if (isHungry){
+    console.log("GO eat!");    
+} else{
+    console.log("You are full!");
+    
+}
+
+//object
+
+let Newcar = {
+    color:"red",
+    Wheels: 4,
+    isElectric: true
+
+};
+console.log(Newcar);
+
+
+//Arrays
+
+let favoriteColors = ["red, white, orange"];
+let score = [100, 80 ,70];
+ console.log(favoriteColors);
+ console.log(score);
+
+ let colours = ["red", "blue", "green", "yellow", "purple"]
+ console.log(colours);
+
+ colours.push("pink")
+ console.log("After adding pink:", colours);
+
+ colours.pop();
+ console.log("After removing the last color:", colours);
+ 
+ 
+ 
+
+ //undefined
+
+ let Toy =  { name: "Teddy Bear" };
+ console.log(Toy);
+ 
+ // Null
+  
+ let lostToy = null;
+ console.log(lostToy);
+
+ //bigint
+
+ let stars = 123456789012345679n;
+ console.log(stars *2n);
+
+ let number = 123456789012345679n;
+ console.log(number);
+ 
+ 
+ 
+ // the flexible Box
+  
+ let Firends = "sourabh"
+ console.log(Firends);
+ Firends = "Abhijeet";
+ console.log(Firends);
+
+ Firends = "Siddharth"
+ console.log(Firends);
+
+ Firends = "sachin"
+ console.log(Firends);
+
+ Firends = "ayub"
+ console.log(Firends);
+
+ Firends = "pallvi"
+ console.log(Firends);
+
+Firends = "Anshika"
+console.log(Firends);
+
+ 
+
